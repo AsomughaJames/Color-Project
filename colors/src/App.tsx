95f76d91a446c560e2e8602ff48b85ced9bf59db
AsomughaJames <asomughajames007@gmail.com>
2022-03-08 21:00:57 +0100

    Added color helper

--- colors/src/App.tsx
+++ colors/src/App.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import './App.css';
 import Pallete from "./components/pallete/pallete"
 import seedColor from "./seedColors"
+import { generatePalette } from "./components/colorHelper"
 
 
 
 function App() {
+  console.log(generatePalette(seedColor))
   return (
     <div className="App">
 
